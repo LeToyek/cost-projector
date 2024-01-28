6d2a369183fb36b188d28fd82bd17d4a309eaa8b
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2024-01-28 14:46:47 +0700

    add tailwind to raw html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ["./src/**/*.{html,js,ts}", "*.{html,js}"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
-
     extend: {},
   },
   plugins: [],
